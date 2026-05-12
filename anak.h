dc5ad5deb8e3ff4f4400e7d3e962a521741cc40b
abiiiaja <kalindraaraya@gmail.com>
2026-05-12 19:53:52 +0700

    deklarasi class anak di file C

---
+++ anak.h
@@ -0,0 +1,24 @@
+#ifndef ANAK_H
+#define ANAK_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//membuat class anak di file header anak.h
+class anak {
+public:
+    //deklarasi member variabel pada class anak
+    string nama;
+
+    //membuat constructor dan destructor pada class anak
+    anak(string pNama) : nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada \n";
+    }
+
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+
+#endif

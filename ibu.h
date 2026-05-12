6db23ec3181b946ba236d21d214db61320e73109
abiiiaja <kalindraaraya@gmail.com>
2026-05-12 20:02:56 +0700

    destructornya ketinggalan

--- ibu.h
+++ ibu.h
@@ -13,9 +13,13 @@ class Ibu{
     public:
     string nama;
     vector<anak*> daftar_anak;
-    
+
     Ibu(string pNama) : nama(pNama) {
         cout << "Ibu \"" << nama << "\" ada\n";
+    };
+
+    ~Ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
     };
 
 };
